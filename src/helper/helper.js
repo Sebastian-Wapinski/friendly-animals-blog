b327c2ec4759edbdfaea1059ed0917cec86b3364
Sebastian <sebastian.pawel.wapinski@gmail.com>
2024-01-08 12:12:41 +0100

    date bug fixed

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -53,7 +53,7 @@ export const returnNewLocation = (location, startDate, endDate) => {
 }
 
 export const createCurrentDate = () => {
-  const date = new Date().getDate()
+  const date = (new Date().getDate()).toString().padStart(2, '0')
   const month = (new Date().getMonth() + 1).toString().padStart(2, '0')
   const year = new Date().getFullYear()
   return `${year}-${month}-${date}`
